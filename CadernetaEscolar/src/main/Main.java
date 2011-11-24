c1b1696774a0ed407ee72865fa3f82935352e6d4
Janse <jansebp@gmail.com>
2011-11-24 04:47:28 -0300

    Main

---
+++ CadernetaEscolar/src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import gui.Autenticacao;
+
+/**
+ * 
+ * @author Fernando Brito			<fernando@lavid.ufpb.br>
+ * @author Jansepetrus Brasileiro	<jansepetrus@lavid.ufpb.br>
+ *
+ */
+public class Main {
+	
+	/**
+	 * Método Inicial da Aplicação.
+	 * 
+	 * @param args Argumentos da linha de comando
+	 */
+	public static void main(String[] args) {
+		new Autenticacao().setVisible(true);
+	}
+}
